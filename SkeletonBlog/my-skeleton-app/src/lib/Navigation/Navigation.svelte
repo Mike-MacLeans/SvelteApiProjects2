fcdd1dff4338391faddc519284b3cd0665d56d52
Mike-MacLeans <mike.maclean400@gmail.com>
2024-11-26 10:43:42 -0600

    Added custom slideFade transition

--- SkeletonBlog/my-skeleton-app/src/lib/Navigation/Navigation.svelte
+++ SkeletonBlog/my-skeleton-app/src/lib/Navigation/Navigation.svelte
@@ -1,18 +1,47 @@
 <script lang="ts">
 	import { getDrawerStore } from '@skeletonlabs/skeleton';
+	import { fade, fly } from 'svelte/transition';
+	import { cubicOut } from 'svelte/easing';
 
 	const drawerStore = getDrawerStore();
 
 	function drawerClose(): void {
 		drawerStore.close();
 	}
+
+	interface SlidefadeParams {
+		delay?: number;
+		duration?: number;
+		easing?: (t: number) => number;
+	}
+
+	export function slidefade(node: HTMLElement, params: SlidefadeParams) {
+		// Get the existing transform property of the node
+		const existingTransform = getComputedStyle(node).transform.replace('none', '');
+
+		return {
+			delay: params.delay || 0,
+			duration: params.duration || 400,
+			easing: params.easing || cubicOut,
+			css: (t: number, u: number) =>
+				`transform-origin: top left; transform: ${existingTransform} scaleY(${t}); opacity: ${t};`
+		};
+	}
 </script>
 
 <nav class="list-nav p-4">
 	<ul>
-		<li><a href="/" on:click={drawerClose}>Homepage</a></li>
-		<li><a href="/about" on:click={drawerClose}>About</a></li>
-		<li><a href="/blog" on:click={drawerClose}>Blog</a></li>
-		<li><a href="/contact" on:click={drawerClose}>Contact</a></li>
+		<li>
+			<a href="/" on:click={drawerClose} transition:slidefade={{ delay: 0 }}>Homepage</a>
+		</li>
+		<li>
+			<a href="/about" on:click={drawerClose} transition:slidefade={{ delay: 75 }}>About</a>
+		</li>
+		<li>
+			<a href="/blog" on:click={drawerClose} transition:slidefade={{ delay: 150 }}>Blog</a>
+		</li>
+		<li>
+			<a href="/contact" on:click={drawerClose} transition:slidefade={{ delay: 225 }}>Contact</a>
+		</li>
 	</ul>
 </nav>
--- SkeletonBlog/my-skeleton-app/src/routes/+layout.svelte
+++ SkeletonBlog/my-skeleton-app/src/routes/+layout.svelte
@@ -26,9 +26,9 @@
 <AppShell slotSidebarLeft="bg-surface-500/5 w-0 lg:w-64">
 	<svelte:fragment slot="header">
 		<!-- App Bar -->
-		<AppBar>
+		<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
 			<svelte:fragment slot="lead">
-				<div class="flex items-center">
+				<div class="flex items-center justify-center">
 					<button class="lg:hidden btn btn-sm mr-4" on:click={drawerOpen}>
 						<span>
 							<svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
@@ -41,6 +41,7 @@
 					<img alt="The project logo" src={logo} />
 				</div>
 			</svelte:fragment>
+
 			<svelte:fragment slot="trail">
 				<LightSwitch />
 			</svelte:fragment>
